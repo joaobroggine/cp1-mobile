05723425027ae75535a8aa855726ae2f7a1d5089
joaobroggine <joaovitorbrogginelopes@gmail.com>
2025-03-11 11:33:31 -0300

    Tratação de excessões feita.

--- cp1mobile/src/pages/form/Form.js
+++ cp1mobile/src/pages/form/Form.js
@@ -1,9 +1,25 @@
-import React from 'react'
-import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import { Alert, Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import Logo from '../../../assets/logo.png'
 import { style } from './Style'
 
 export default function Form() {
+
+    const [nome, setNome] = useState('')
+    const [valor, setValor] = useState('')
+    const [percentual, setPercentual] = useState('')
+
+    async function getCalculo() {
+        try {
+            if (!nome || !valor || !percentual) {
+                return Alert.alert("Preencha os campos obrigatórios.", "Todos os campos devem ser preenchidos")
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
     return (
         <View style={style.container}>
             <View style={style.titleBox}>
@@ -19,22 +35,30 @@ export default function Form() {
                     <View style={style.inputBox}>
                         <TextInput
                             style={style.input}
+                            value={nome}
+                            onChangeText={setNome}
                         />
                         <Text style={style.inputText}>VALOR ORIGINAL DO PRODUTO</Text>
                         <TextInput
                             style={style.input}
+                            keyboardType='numeric'
+                            value={valor}
+                            onChangeText={setValor}
                         />
                     </View>
                     <View style={style.inputBox}>
-                        <Text style={style.inputText}>PORCENTUAL GANHO</Text>
+                        <Text style={style.inputText}>PERCENTUAL GANHO</Text>
                         <TextInput
                             style={style.input}
+                            keyboardType='numeric'
+                            value={percentual}
+                            onChangeText={setPercentual}
                         />
                     </View>
                 </View>
             </View>
             <View style={style.buttonBox}>
-                <TouchableOpacity style={style.button}>
+                <TouchableOpacity style={style.button} onPress={() => getCalculo()}>
                     <Text style={style.buttonText}>Calcular</Text>
                 </TouchableOpacity>
             </View>
